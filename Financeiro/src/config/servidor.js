03eed66d46e297ac92ef91df56311944c4e96cb4
Marinaldo Ferreira <marinaldofsilva@gmail.com>
2022-07-10 18:39:14 -0300

    Alterações

--- Financeiro/src/config/servidor.js
+++ Financeiro/src/config/servidor.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 module.exports = {
     port: 11901, jwt_secret: "#V$Code%", jwt_expires: "2d", salt: 10, connect: () => {
-        const db_path = "mongodb+srv://naldo:abc123321@cluster0.juk7d.mongodb.net/db_users?retryWrites=true&w=majority"
+        const db_path = "mongodb://localhost:27017/db_financeiro?retryWrites=true&w=majority"
         mongoose.connect(db_path, {
             useNewUrlParser: true, useUnifiedTopology: true
         }, () => console.log('[MS_USERS] Banco conectado'));
